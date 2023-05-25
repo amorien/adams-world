feb47da85052b84d615743357ed8c5b083eda1ce
amorien <110195681+amorien@users.noreply.github.com>
2023-05-25 01:55:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# adams-world
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-nnu8z9)
---
+++ src/components/adamsworld.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+     Adam's <code>Web3.0</code> Vue application rendoring Vite interactive components. 
+     <p> <code>components/adamsworld.vue</code> for Vite POC</p>
+    </p>
+  </div>
+
+  <p>
+    Powered 
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite source 
+  </p>
+  <p>
+    Integrated with
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    IDE for a better DX
+  </p>
+  <p class="read-the-docs">Good DX means DevOps can seamlessly <code>package.json</code> this technology within their projects. This interoperable framework is flexible to evaluate, install, test, integrate, and maintain cross-platform W3C standardization.</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
