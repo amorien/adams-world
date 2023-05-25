13a8e0ece6d7fbcc6c7375637d5de70c7cab1617
amorien <110195681+amorien@users.noreply.github.com>
2023-05-25 02:41:57 -0700

    2 files modified

--- src/components/adamsworld.vue
+++ src/components/adamsworld.vue
@@ -10,27 +10,27 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-     Adam's <code>Web3.0</code> Vue application rendoring Vite interactive components. 
-     <p> <code>components/adamsworld.vue</code> for Vite POC</p>
-    </p>
-  </div>
-
-  <p>
-    Powered 
+ <p>
+    Powered by
     <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
       >create-vue</a
-    >, the official Vue + Vite source 
+    >, the official Vue + Vite source. 
   </p>
   <p>
     Integrated with
     <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    IDE for a better DX
+    IDE for a better DX.
   </p>
   <p class="read-the-docs">Good DX means DevOps can seamlessly <code>package.json</code> this technology within their projects. This interoperable framework is flexible to evaluate, install, test, integrate, and maintain cross-platform W3C standardization.</p>
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+     Adam's <code>Web3.0</code> Vue app rendering Vite interactive components. 
+     <p> Visit <code>components/adamsworld.vue</code> for Vue proof of concept.</p>
+    </p>
+  </div>
+
+ 
 </template>
 
 <style scoped>
